8c1af2fc5be9ac3c79a47cefa31705fdc7732a53
WeRockStar <vr_freestyle@outlook.com>
2016-09-14 22:23:54 +0700

    Bind cameraview

--- app/src/main/java/com/werockstar/androidfacetracker/MainActivity.java
+++ app/src/main/java/com/werockstar/androidfacetracker/MainActivity.java
@@ -3,11 +3,31 @@ package com.werockstar.androidfacetracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.cameraview.CameraView;
+
 public class MainActivity extends AppCompatActivity {
+
+    CameraView mCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCameraView = (CameraView) findViewById(R.id.cameraview);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mCameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mCameraView.stop();
     }
 }
